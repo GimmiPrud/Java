98ba930b667e36197f80dbfafdde75715a78b2c8
GimmiPrud <prudenzig@gmail.com>
2024-10-11 10:51:01 +0200

    ...

--- java_eclipse-workspace/P2024107_01_Secondo/src/Pitagora.java
+++ java_eclipse-workspace/P2024107_01_Secondo/src/Pitagora.java
@@ -1,13 +1,21 @@
-
 public class Pitagora {
+    public static void main(String[] args) {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		
-		double c1 = 45.3;
-		double c2 = 67.2;
-		
-		double i = (45.3 * 2) + (67.2 * 2);
-	}
+        double c1 = 45.3;
+        double c2 = 67.2;
 
+        double Ipo = Math.sqrt((c1*c1)+(c2*c2));
+        double Area = (c1*c2)/2;
+        double P = Ipo + c1 + c2;
+        double r_cerchio = (c1/c2)*Ipo;
+        double C_cerchio = (3.14 * r_cerchio)*2;
+        double A_cerchio = (r_cerchio*r_cerchio)* 3.14;
+
+        System.out.println("Ipotenusa = " + Ipo);
+        System.out.println("Area = " + Area);
+        System.out.println("Perimetro = " + P);
+        System.out.println("Circonferenza cerchio = " + C_cerchio);
+        System.out.println("Area cerchio = " + A_cerchio);
+
+    }
 }
