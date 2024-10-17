5f21b13518f112e86a31d10680a28ef7753aafbb
GimmiPrud <prudenzig@gmail.com>
2024-10-17 18:24:46 +0200

    .....

--- java_eclipse-workspace/P_20241011_Esercizio/src/Computer.java
+++ java_eclipse-workspace/P_20241011_Esercizio/src/Computer.java
@@ -1,20 +1,102 @@
 
+// Creazione classi e costruttori in java (getter and setter)
 public class Computer {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	
+	int n_computer = 0;
+   
+	private double prezzo;
+	private double peso;
+	private double larghezza;
+	private double altezza;
+	private double profondita;
+	private String produttore;
+	private int annoProduzione;
+	
+	// costruttore senza parametri 
+	public Computer() {
+	}
+	
+	// costruttore con parametri 
+	public Computer(double prezzo, double peso, double larghezza, double altezza, double profondita,
+			String produttore, int annoProduzione) 
+	{	
+		this.setPrezzo(prezzo);
+		this.setPeso(peso);
+		this.setLarghezza(larghezza);
+		this.setAltezza(altezza);
+		this.setProfondita(profondita);
+		this.setProduttore(produttore);
+		this.setAnnoProduzione(annoProduzione);
 		
-		double Prezzo = 999.99;
-		float Peso = 2.5F;
-		double Larghezza = 34.5;
-		double Altezza = 3;
-		double Profondità = 24;
-		String Produttore = "HP";
-		int Anno_prod = 2023;
-
-	}	
-
+	}
 	
-	
+   public static void main(String[] args) {
+	   // TODO Auto-generated method stub
+	   
+	   // creazione degli oggetti di tipo computer 
+	   Computer computer1 = new Computer();
+	   Computer computer2 = new Computer();
+   }
+   
+   //
+   public double getPrezzo() {
+	   return prezzo;
+   }
+
+   public void setPrezzo(double prezzo) {
+	   this.prezzo = prezzo;
+   }
+
+   public double getPeso() {
+	   return peso;
+   }
+
+   public void setPeso(double peso) {
+	   this.peso = peso;
+   }
+
+   public double getLarghezza() {
+	   return larghezza;
+   }
+
+   public void setLarghezza(double larghezza) {
+	   this.larghezza = larghezza;
+   }
+
+   public double getAltezza() {
+	   return altezza;
+   }
+
+   public void setAltezza(double altezza) {
+	   this.altezza = altezza;
+   }
+
+   public double getProfondita() {
+	   return profondita;
+   }
+
+   public void setProfondita(double profondita) {
+	   this.profondita = profondita;
+   }
+
+   public String getProduttore() {
+	   return produttore;
+   }
+
+   public void setProduttore(String produttore) {
+	   this.produttore = produttore;
+   }
+
+   public int getAnnoProduzione() {
+	   return annoProduzione;
+   }
+
+   public void setAnnoProduzione(int annoProduzione) {
+	   this.annoProduzione = annoProduzione;
+   }
+   
+   
 }
+
+
 
