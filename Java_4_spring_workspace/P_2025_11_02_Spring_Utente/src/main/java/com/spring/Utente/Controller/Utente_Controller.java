444509d99acf2d247bc1c97a9ddbc4f093a6d0ef
GimmiPrud <prudenzig@gmail.com>
2025-03-05 10:58:32 +0100

    ...

--- Java_4_spring_workspace/P_2025_11_02_Spring_Utente/src/main/java/com/spring/Utente/Controller/Utente_Controller.java
+++ Java_4_spring_workspace/P_2025_11_02_Spring_Utente/src/main/java/com/spring/Utente/Controller/Utente_Controller.java
@@ -3,13 +3,20 @@ package com.spring.Utente.Controller;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.spring.Utente.Dto.ErroreDto;
 import com.spring.Utente.Dto.NomeCognomeDTO;
 import com.spring.Utente.Dto.UtenteDTO;
 import com.spring.Utente.Service.Utente_Service;
@@ -33,7 +40,7 @@ import com.spring.Utente.entity.Utente; // importiamo la classe utente
 		
 	}
 
-	@GetMapping(path = "/cerca/{id}", produces = "application/Json")
+	@GetMapping(path = "/{id}", produces = "application/Json")
 	public UtenteDTO cercaPerId(@PathVariable int id) {
 	//	return new Utente(id, "mario", "rossi", "mario", "red");
 		
@@ -43,25 +50,32 @@ import com.spring.Utente.entity.Utente; // importiamo la classe utente
 
 
 	// creare una lista che si aggiorna per ogni utente registrato e lo manda a schermo
-	@GetMapping(path = "/mostraTutti", produces = MediaType.APPLICATION_JSON_VALUE) 
+	@GetMapping(path = "", produces = MediaType.APPLICATION_JSON_VALUE) 
 	public List<UtenteDTO> mostraTutti(){
 		return service.mostraTutti();
 		
 	}
 	
-	@GetMapping(path="/modificaPassword/{idUtente}", produces = "application/json")
+	@PatchMapping(path="/{idUtente}", produces = "application/json")
 	public UtenteDTO modificaMail(@PathVariable int idUtente, String password) {
 		return service.updatePassword(idUtente, password);
 	}
 	
-	@GetMapping(path="/cancella/{idUtente}", produces = MediaType.APPLICATION_JSON_VALUE)
+	@DeleteMapping(path="/{idUtente}", produces = MediaType.APPLICATION_JSON_VALUE)
 	public UtenteDTO cancellaUtente(@PathVariable int idUtente) {
 		return service.cancella(idUtente);
 	}
 
-	@GetMapping(path="/nomeCognome/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+	@GetMapping(path="/{id}/nomeCognome", produces = MediaType.APPLICATION_JSON_VALUE)
 	public NomeCognomeDTO getNomeCognome(@PathVariable int id) {
 		return service.getNomeCognome(id);
+	}
+	
+	@ExceptionHandler
+	public ResponseEntity<ErroreDto> gestore(RuntimeException exc){
+		ErroreDto errore = new ErroreDto(exc.getMessage());
+		ResponseEntity<ErroreDto> response = new ResponseEntity<ErroreDto>(errore, HttpStatus.NOT_FOUND);
+		return response;
 	}
 	
 }
